54f61bc8ad97967bf606c4d0a98b218b659de938
Rajdev-ullash <rajdevullash111@gmail.com>
2022-03-16 09:16:51 +0600

    complete

---
+++ nav.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-sm bg-light">
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="index.php">Create Department </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="departments.php">Department info</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="employee.php">Create Employee</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="employees.php">Employees list</a>
+    </li>
+
+  </ul>
+</nav>
